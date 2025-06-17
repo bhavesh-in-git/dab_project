b010bc9380590ae1c290d1ba6e59327878d0f645
Bhavesh Patel <bhaveshin@yahoo.com>
2025-06-17 14:03:05 -0400

    Made changes to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.2",
-    description="This contains the code in the .\src directory of the project",
-    package=find_packages(where=".\src"),
-    package_dir={"":".\src"},
+    version="0.0.1",
+    description="This contains the code in the ./src directory of the project",
+    packages=find_packages(where="./src"),
+    package_dir={"": "./src"},
     install_requires=["setuptools"],
     entry_points={
-        "packages":[
+        "console_scripts": [
             "main=dab_project.main:main"
         ]
     }
