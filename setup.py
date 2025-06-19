36084d037236090290af853a4a42d85b5ff99ff0
Bhavesh Patel <bhaveshin@yahoo.com>
2025-06-19 09:21:41 -0400

    Changed Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.2",
+    version="0.0.3",
     description="This contains the code in the ./src directory of the project",
     packages=find_packages(where="./src"),
     package_dir={"": "./src"},
